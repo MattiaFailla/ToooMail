d108d39d72ea4c21bbf9713f4acf653d432a7667
Mattia <mattiafailla@hotmail.it>
2019-08-30 02:48:47 +0200

    Black test reformat

--- test.py
+++ test.py
@@ -3,17 +3,17 @@ from app import *
 
 from py_modules import db_api
 
-class TestStringMethods(unittest.TestCase):
 
+class TestStringMethods(unittest.TestCase):
     def test__user_info(self):
-        self.assertEqual(verify_user_info("false","false","false"), False)
+        self.assertEqual(verify_user_info("false", "false", "false"), False)
 
     def test__user_exist(self):
         self.assertEqual(check_if_user_exists(), "registration.html")
 
     def test__data_get(self):
-        self.assertEqual(db_api.get("user","id","WHERE ID = 1"), [])
+        self.assertEqual(db_api.get("user", "id", "WHERE ID = 1"), [])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
-
