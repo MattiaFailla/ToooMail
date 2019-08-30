c630e1c9920f1eed076a1f55aa1ba178b6b44301
Mattia <mattiafailla@hotmail.it>
2019-08-30 02:48:18 +0200

    Adding black codestyle to py_modules

--- py_modules/db_api.py
+++ py_modules/db_api.py
@@ -1,4 +1,5 @@
 import sqlite3
+
 """
 Struttura db
 --- EMAILS
@@ -50,14 +51,15 @@ Api
 DB_LOCATION = "db/app.db"
 
 conn = sqlite3.connect(DB_LOCATION)
-#c = conn.cursor()
+# c = conn.cursor()
 
 conn = sqlite3.connect(DB_LOCATION)
 c = conn.cursor()
 
-#--- emails table
-#create table
-c.execute('''CREATE TABLE IF NOT EXISTS emails (
+# --- emails table
+# create table
+c.execute(
+    """CREATE TABLE IF NOT EXISTS emails (
  uid integer PRIMARY KEY,
  from_name text NOT NULL,
  from_mail text NOT NULL,
@@ -67,36 +69,42 @@ c.execute('''CREATE TABLE IF NOT EXISTS emails (
  bodyHTML text,
  bodyPLAIN text,
  directory text,
- datetimes datetime);''')
-#commit the changes to db			
+ datetimes datetime);"""
+)
+# commit the changes to db
 conn.commit()
 
 
-#--- unreads table
-#create table
-c.execute('''CREATE TABLE IF NOT EXISTS unreads (
+# --- unreads table
+# create table
+c.execute(
+    """CREATE TABLE IF NOT EXISTS unreads (
  id integer PRIMARY KEY,
- uid int NOT NULL);''')
-#commit the changes to db			
+ uid int NOT NULL);"""
+)
+# commit the changes to db
 conn.commit()
 
 
-#--- sent table
-#create table
-c.execute('''CREATE TABLE IF NOT EXISTS sent (
+# --- sent table
+# create table
+c.execute(
+    """CREATE TABLE IF NOT EXISTS sent (
  id integer PRIMARY KEY,
  to_mail text NOT NULL,
  subject text,
  body text,
  attach text,
- datetimes datetime);''')
-#commit the changes to db			
+ datetimes datetime);"""
+)
+# commit the changes to db
 conn.commit()
 
 
-#--- user table
-#create table
-c.execute('''CREATE TABLE IF NOT EXISTS user (
+# --- user table
+# create table
+c.execute(
+    """CREATE TABLE IF NOT EXISTS user (
  id integer PRIMARY KEY,
  name text NOT NULL,
  surname text,
@@ -108,27 +116,31 @@ c.execute('''CREATE TABLE IF NOT EXISTS user (
  imapserver text,
  smtpserver text,
  datetime datetime
-);''')
-#commit the changes to db			
+);"""
+)
+# commit the changes to db
 conn.commit()
 
 
-#--- notes table
-#create table
-c.execute('''CREATE TABLE IF NOT EXISTS notes (
+# --- notes table
+# create table
+c.execute(
+    """CREATE TABLE IF NOT EXISTS notes (
  id integer PRIMARY KEY,
  uid text NOT NULL,
  note text,
  files text,
  datetime datetime
-);''')
-#commit the changes to db			
+);"""
+)
+# commit the changes to db
 conn.commit()
 
 
-#--- contacts table
-#create table
-c.execute('''CREATE TABLE IF NOT EXISTS contacts (
+# --- contacts table
+# create table
+c.execute(
+    """CREATE TABLE IF NOT EXISTS contacts (
  id integer PRIMARY KEY,
  name text NOT NULL,
  surname text,
@@ -136,24 +148,29 @@ c.execute('''CREATE TABLE IF NOT EXISTS contacts (
  mail text NOT NULL,
  note text,
  datetime datetime
-);''')
-#commit the changes to db			
+);"""
+)
+# commit the changes to db
 conn.commit()
 
 
-#--- emails table
-#create table
-c.execute('''CREATE TABLE IF NOT EXISTS last_uid (
- uid integer PRIMARY KEY);''')
-#commit the changes to db			
+# --- emails table
+# create table
+c.execute(
+    """CREATE TABLE IF NOT EXISTS last_uid (
+ uid integer PRIMARY KEY);"""
+)
+# commit the changes to db
 conn.commit()
 
 
-#--- emails table
-#create table
-c.execute('''CREATE TABLE IF NOT EXISTS starred (
- uid integer PRIMARY KEY);''')
-#commit the changes to db			
+# --- emails table
+# create table
+c.execute(
+    """CREATE TABLE IF NOT EXISTS starred (
+ uid integer PRIMARY KEY);"""
+)
+# commit the changes to db
 conn.commit()
 """
 FIRST RUN
@@ -163,19 +180,21 @@ OTHER RUN
 Check for new mails -> send in-app notification -> save in the db -> notify the frontend 
 """
 
-			
-#close the connection
-#conn.close()
+
+# close the connection
+# conn.close()
 
 """ BASIC SQLITE FUNCTIONS """
-def insert(table,data):
-	cols = ', '.join('"{}"'.format(col) for col in data.keys())
-	vals = ', '.join(':{}'.format(col) for col in data.keys())
-	sql = 'INSERT INTO "{0}" ({1}) VALUES ({2})'.format(table, cols, vals)
-	conn.cursor().execute(sql, data)
-	conn.commit()
-	return True
-	"""example
+
+
+def insert(table, data):
+    cols = ", ".join('"{}"'.format(col) for col in data.keys())
+    vals = ", ".join(":{}".format(col) for col in data.keys())
+    sql = 'INSERT INTO "{0}" ({1}) VALUES ({2})'.format(table, cols, vals)
+    conn.cursor().execute(sql, data)
+    conn.commit()
+    return True
+    """example
 	sqlite_insert(conn, 'stocks', {
 		'created_at': '2016-04-17',
 		'type': 'BUY',
@@ -183,26 +202,30 @@ def insert(table,data):
 		'price': 45.00})
 	"""
 
-def update(table,where,data):
-	pass
 
-def delete(table,where,what):
-	cur = conn.cursor()
-	cur.execute("DELETE FROM "+table+" WHERE "+where+" LIKE %"+what+"%")
-	cur.commit()
+def update(table, where, data):
+    pass
+
+
+def delete(table, where, what):
+    cur = conn.cursor()
+    cur.execute("DELETE FROM " + table + " WHERE " + where + " LIKE %" + what + "%")
+    cur.commit()
+
 
 def drop(table):
-	cur = conn.cursor()
-	cur.execute("DROP TABLE "+table+"")
-	cur.commit()
+    cur = conn.cursor()
+    cur.execute("DROP TABLE " + table + "")
+    cur.commit()
 
-def get(table,field,expressions):
-	cur = conn.cursor()
-	cur.execute("SELECT "+field+" FROM "+table+" "+expressions+"")
 
-	rows = cur.fetchall()
-	data = []
-	for row in rows:
-		data.append(row)
+def get(table, field, expressions):
+    cur = conn.cursor()
+    cur.execute("SELECT " + field + " FROM " + table + " " + expressions + "")
 
-	return data
+    rows = cur.fetchall()
+    data = []
+    for row in rows:
+        data.append(row)
+
+    return data
