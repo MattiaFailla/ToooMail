03751a9a5f65240c83a70ac74d2fd0da944258c2
Mattia <mattiafailla@hotmail.it>
2019-12-24 01:57:24 +0100

    Creating downloadModule

---
+++ py_modules/download_module.py
@@ -0,0 +1,27 @@
+class downloadModule():
+    userId = 0
+    userName = ""
+
+    def __init__(self, userName, userId):
+        self.userId = userId
+        self.userName = userName
+
+    def downloadUUID(self):
+        """downloadUUID
+
+        download every UUID and save them on the db
+
+        """
+        return
+
+    def getFolders(self):
+        """getFolders
+
+
+        download every folder name
+        return -> ids of folders with name
+
+
+        """
+        return
+
