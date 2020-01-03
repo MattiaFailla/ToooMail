6e3d97ac5545d88885686470a1fcf4876a08c705
Mattia <mattiafailla@hotmail.it>
2020-01-03 18:23:51 +0100

    Improving performances in table creation

--- py_modules/db_api.py
+++ py_modules/db_api.py
@@ -39,117 +39,83 @@ class DBApi:
 
 
 ### CREATE ALL THE TABLES
+
 DB_LOCATION = ".db/app.db"
-
-conn = sqlite3.connect(DB_LOCATION)
-# c = conn.cursor()
-
 conn = sqlite3.connect(DB_LOCATION)
 c = conn.cursor()
 
-# --- user table
-# create table
-c.execute(
-    """CREATE TABLE IF NOT EXISTS user (
- id integer PRIMARY KEY,
- name text NOT NULL,
- surname text,
- nickname text,
- bio text,
- mail text,
- password text,
- profilepic text,
- imapserver text,
- smtpserver text,
- is_logged_in integer,
- mail_server_setting,
- datetime datetime
-);"""
-)
-# commit the changes to db
+queries = {"create_table":
+               """CREATE TABLE IF NOT EXISTS user (
+            id integer PRIMARY KEY,
+            name text NOT NULL,
+            surname text,
+            nickname text,
+            bio text,
+            mail text,
+            password text,
+            profilepic text,
+            imapserver text,
+            smtpserver text,
+            is_logged_in integer,
+            mail_server_setting,
+            datetime datetime
+           );""",
+
+           "mail_server_settings":
+               """CREATE TABLE IF NOT EXISTS mail_server_settings (
+            id integer PRIMARY KEY,
+            service_name text NOT NULL,
+            server_smtp text,
+            server_imap text,
+            ssl text,
+            ssl_context text,
+            starttls text
+           );""",
+           "emails_table":
+               """CREATE TABLE IF NOT EXISTS mails (
+            id integer PRIMARY KEY,
+            uuid text NOT NULL,
+            subject text,
+            user_id text,
+            datetime datetime
+           );""",
+           "notes_table":
+               """CREATE TABLE IF NOT EXISTS notes (
+            id integer PRIMARY KEY,
+            uid text NOT NULL,
+            note text,
+            files text,
+            datetime datetime
+           );""",
+           "contacts_table":
+               """CREATE TABLE IF NOT EXISTS contacts (
+            id integer PRIMARY KEY,
+            name text NOT NULL,
+            surname text,
+            nick text,
+            mail text NOT NULL,
+            note text,
+            datetime datetime
+           );""",
+           "files_table":
+               """CREATE TABLE IF NOT EXISTS files (
+                    id integer PRIMARY KEY,
+                    uuid text NOT NULL,
+                    subject text,
+                    real_filename text,
+                    saved_as text,
+                    user_id text,
+                    deleted text,
+                    datetime datetime
+               );"""
+           }
+
+# execute queries
+for query in queries.items():
+    c.execute(query[1])
+
+# saves result
 conn.commit()
-
-# --- mail_server_settings table
-# create table
-c.execute(
-    """CREATE TABLE IF NOT EXISTS mail_server_settings (
- id integer PRIMARY KEY,
- service_name text NOT NULL,
- server_smtp text,
- server_imap text,
- ssl text,
- ssl_context text,
- starttls text
-);"""
-)
-# commit the changes to db
-conn.commit()
-
-# --- mail table
-# create table
-c.execute(
-    """CREATE TABLE IF NOT EXISTS mails (
- id integer PRIMARY KEY,
- uuid text NOT NULL,
- subject text,
- user_id text,
- datetime datetime
-);"""
-)
-# commit the changes to db
-conn.commit()
-
-# --- notes table
-# create table
-c.execute(
-    """CREATE TABLE IF NOT EXISTS notes (
- id integer PRIMARY KEY,
- uid text NOT NULL,
- note text,
- files text,
- datetime datetime
-);"""
-)
-# commit the changes to db
-conn.commit()
-
-# --- contacts table
-# create table
-c.execute(
-    """CREATE TABLE IF NOT EXISTS contacts (
- id integer PRIMARY KEY,
- name text NOT NULL,
- surname text,
- nick text,
- mail text NOT NULL,
- note text,
- datetime datetime
-);"""
-)
-# commit the changes to db
-conn.commit()
-
-# --- files table
-# create table
-c.execute(
-    """CREATE TABLE IF NOT EXISTS files (
-         id integer PRIMARY KEY,
-         uuid text NOT NULL,
-         subject text,
-         real_filename text,
-         saved_as text,
-         user_id text,
-         deleted text,
-         datetime datetime
-    );"""
-)
-# commit the changes to db
-conn.commit()
-
-# close the connection
-# conn.close()
-
-""" BASIC SQLITE FUNCTIONS """
 
 """ UPLOADING APP SETTINGS """
 with open(".db/mail_server.json", "r") as f:
