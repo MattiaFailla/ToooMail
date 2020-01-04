8db823e011c3a79e6eff13d1fd036a343a9f4395
Mattia <mattiafailla@hotmail.it>
2020-01-04 19:48:09 +0100

    Adding comments in the table creation
    + Adding the field app_password in the user_table
    + Fixing uuid field name in notes_table

--- py_modules/db_api.py
+++ py_modules/db_api.py
@@ -47,46 +47,48 @@ c = conn.cursor()
 
 queries = {"create_table":
                """CREATE TABLE IF NOT EXISTS user (
-            id integer PRIMARY KEY,
-            name text NOT NULL,
-            surname text,
-            nickname text,
-            bio text,
-            mail text,
-            password text,
-            profilepic text,
-            imapserver text,
-            smtpserver text,
-            is_logged_in integer,
-            mail_server_setting,
-            datetime datetime
+            id integer PRIMARY KEY, -- unique user id
+            name text NOT NULL, -- user name
+            surname text, -- user surname (optional)
+            nickname text, -- user nickname (optional)
+            bio text, -- user bio (optional)
+            mail text, -- user email address
+            password text, -- user email password
+            app_password text, -- app password to unlock (optional)
+            profilepic text, -- user profile pic ((optional), must contain the local file reference)
+            imapserver text, -- imap server address
+            smtpserver text, -- smtp server address
+            is_logged_in integer, -- flag to check if the user is logged in the app
+            mail_server_setting, -- external id (mail_server_settings)
+            datetime datetime -- datetime of the user registration
            );""",
 
            "mail_server_settings":
                """CREATE TABLE IF NOT EXISTS mail_server_settings (
-            id integer PRIMARY KEY,
-            service_name text NOT NULL,
-            server_smtp text,
-            server_imap text,
-            ssl text,
-            ssl_context text,
-            starttls text
+            id integer PRIMARY KEY, -- unique setting entry id
+            service_name text NOT NULL, -- display name of the service
+            server_smtp text, -- smtp server address
+            server_imap text, -- imap server address 
+            ssl text, -- ssl setting
+            ssl_context text, -- ssl_context setting
+            starttls text -- starttls setting
            );""",
            "emails_table":
                """CREATE TABLE IF NOT EXISTS mails (
-            id integer PRIMARY KEY,
-            uuid text NOT NULL,
-            subject text,
-            user_id text,
-            datetime datetime
+            id integer PRIMARY KEY, -- the entry id
+            uuid text NOT NULL, -- the mail uuid
+            subject text, -- the mail subject
+            user_id text, -- the user id
+            opened integer, -- flag to check if email has been opened
+            datetime datetime -- datetime from the imap server
            );""",
            "notes_table":
                """CREATE TABLE IF NOT EXISTS notes (
-            id integer PRIMARY KEY,
-            uid text NOT NULL,
-            note text,
-            files text,
-            datetime datetime
+            id integer PRIMARY KEY, -- the note entry id
+            uuid text NOT NULL, -- the mail uuid
+            note text, -- the note body
+            files text, -- attached file, must contain fs reference
+            datetime datetime -- datetime of the last saved note
            );""",
            "contacts_table":
                """CREATE TABLE IF NOT EXISTS contacts (
@@ -101,13 +103,13 @@ queries = {"create_table":
            "files_table":
                """CREATE TABLE IF NOT EXISTS files (
                     id integer PRIMARY KEY,
-                    uuid text NOT NULL,
-                    subject text,
-                    real_filename text,
-                    saved_as text,
-                    user_id text,
-                    deleted text,
-                    datetime datetime
+                    uuid text NOT NULL, -- the mail uuid
+                    subject text, -- the mail subject (redundant)
+                    real_filename text, -- real filename 
+                    saved_as text, -- filename in the local fs
+                    user_id text, -- the user id (owner of the file)
+                    deleted text, -- flag to indicate if the file has been gracefully deleted
+                    datetime datetime -- datetime from server
                );"""
            }
 
