d898cdff976665b1485b23064a743e1238f3e6db
Mattia <mattiafailla@hotmail.it>
2020-04-02 21:53:10 +0200

    Pushing the Vue UI sample template

---
+++ ui/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import store from './store'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
