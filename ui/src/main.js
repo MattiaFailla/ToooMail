497aa7745e71f1976dd4a8b1d2ef0405875e2261
Mattia <mattiafailla@hotmail.it>
2020-04-05 00:10:38 +0200

    Pushing the Vue simple project

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,15 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
-import store from './store'
-import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  store,
-  vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ ui/vue.config.js
@@ -0,0 +1,4 @@
+const path = require('path');
+module.exports = {
+    outputDir: "../web" // or whatever you would like your output directory to be, which will be used in the next step.
+};
