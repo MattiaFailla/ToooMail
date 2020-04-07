cb68830ca0ba13b446a6bc2b8482e4abba81efe6
Mattia <mattiafailla@hotmail.it>
2020-04-07 15:10:56 +0200

    Adding element-ui

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// importing UI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 // handling cookies
 import VueCookies from 'vue-cookies';
 Vue.use(VueCookies);
